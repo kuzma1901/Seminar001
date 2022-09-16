b22feb0b090082c28be171a877b4d8cc37ca10d6
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-09-16 15:51:50 +0300

    ADD files seminars

---
+++ C#/seminar003/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 7)
+{
+    Console.Write("Вы ввели некорректное значение :( ");
+}
+if (num == 1)
+{
+    Console.Write("Это понедельник");
+}
+if (num == 2)
+{
+    Console.Write("Это вторник");
+}
+if (num == 3)
+{
+    Console.Write("Это среда");
+}
+if (num == 4)
+{
+    Console.Write("Это четверг");
+}
+if (num == 5)
+{
+    Console.Write("Это пятница");
+}
+if (num == 6)
+{
+    Console.Write("Это суббота");
+}
+if (num == 7)
+{
+    Console.Write("Это воскресенье");
+}
