e2b1a7e91510b962967a683c80ff11cf8e755dfd
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-09-19 21:19:27 +0300

    Add Seminar2

---
+++ Seminar2/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит
+// 1. случайное число из отрезка [10, 99]
+// 2. наибольшую цифру числа.
+// 3. вывод результата
+
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+Console.WriteLine($"случайное число отрезка 10-99 это {number}");
+if(firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {number} => {firstDigit}");
+else Console.WriteLine($"наибольшая цифра числа {number} => {secondDigit}");
