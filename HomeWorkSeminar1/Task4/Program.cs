a843b1dc91ef6248062ec7a4b0529d50c2656f07
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-09-21 16:06:51 +0300

    переименовал папку с ДЗ

---
+++ HomeWorkSeminar1/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введи 3 числа и я скажу какое из них большее");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.Write($"число {a} - большее");
+}
+if (b > a && b > c)
+{
+    Console.Write($"число {b} - большее");
+}
+if (c > a && c > b)
+{
+    Console.Write($"число {c} - большее");
+}
---
+++ HomeWorkSeminar1/Task8/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введи число и я скажу все четные числа до него");
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine()); 
+for (int a = 1; a < x+1; a++)       
+if (a%2==0)            
+Console.WriteLine(a);
