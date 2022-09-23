e96c3f2d6c1b03c775f2beedbde7b23a7e779eee
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-09-23 13:28:18 +0300

    Add HomeWorkSeminar2/

---
+++ HomeWorkSeminar2/Task15/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру, обозначающую день недели и я скажу является ли он выходным");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 7)
+{
+    Console.Write("Вы ввели некорректное значение :( ");
+}
+if (num == 1)
+{
+    Console.Write("нет");
+}
+if (num == 2)
+{
+    Console.Write("нет");
+}
+if (num == 3)
+{
+    Console.Write("нет");
+}
+if (num == 4)
+{
+    Console.Write("нет");
+}
+if (num == 5)
+{
+    Console.Write("нет");
+}
+if (num == 6)
+{
+    Console.Write("да");
+}
+if (num == 7)
+{
+    Console.Write("да");
+}
