35887875d810c3b904f12cee9759ee62206a6592
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-09-26 13:52:56 +0300

    Seminar3

---
+++ Seminar3/task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+
+Console.WriteLine("Введите координаты точки");
+Console.Write ("x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write ("y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
