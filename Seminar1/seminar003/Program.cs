724b8e0c8aea1ad35d71dbf232036803ef922417
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-04 14:08:39 +0300

    скорректировал задачу3

--- Seminar1/seminar003/Program.cs
+++ Seminar1/seminar003/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число от 1 до 7");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num > 7)
+if (num > 7 || num < 1)
 {
     Console.Write("Вы ввели некорректное значение :( ");
 }
