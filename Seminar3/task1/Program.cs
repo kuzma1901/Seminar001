99728c68d00909b1ecbe2c2b65bd3f063067af92
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-04 14:09:15 +0300

    Add seminar3

--- Seminar3/task1/Program.cs
+++ Seminar3/task1/Program.cs
@@ -3,9 +3,21 @@
 // номер четверти плоскости, в которой находится эта
 
 Console.WriteLine("Введите координаты точки");
-Console.Write ("x: ");
+Console.Write("x: ");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.Write ("y: ");
+Console.Write("y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
+int quarter = Quarter (x, y);
+string result = quarter > 0 ? $"Указанные координаты соответсвуют четверти -> {quarter.ToString()}"
+                            : "Введены некорректные координаты";
+Console.WriteLine(result);
 
+int Quarter (int xc, int yc)
+{
+    if(xc > 0 && yc > 0) return 1;
+    if(xc < 0 && yc < 0) return 2;
+    if(xc < 0 && yc > 0) return 3;
+    if(xc > 0 && yc < 0) return 4;
+    return 0;
+}
