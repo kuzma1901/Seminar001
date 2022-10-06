b2444b5b436b35153a8499320c0c249e918c109b
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-06 16:25:22 +0300

    скорректировал ДЗ

--- HomeWorkSeminar2/Task13/Program.cs
+++ HomeWorkSeminar2/Task13/Program.cs
@@ -3,9 +3,11 @@
 Console.Write("Введи число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 string b = Convert.ToString(a);
-if (b.Length > 2){
+if (b.Length > 2)
+{
   Console.WriteLine("третья цифра -> " + b[2]);
 }
-else {
+else 
+{
   Console.WriteLine("-> третьей цифры нет");
 }
