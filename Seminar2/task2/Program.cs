1f68df40fd29362dc4d06e781a1e774c714ddfde
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-06 16:25:50 +0300

    скорректировал семинар2

--- Seminar2/task2/Program.cs
+++ Seminar2/task2/Program.cs
@@ -5,3 +5,11 @@
 // 782 -> 72
 // 918 -> 98
 
+int num = new Random().Next(100, 999);
+Console.WriteLine($"Случайное трехзначное число = {num}");
+
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+Console.Write($"Получившееся число без второй цифры: ");
+Console.Write($"{firstDigit}");
+Console.Write($"{thirdDigit}");
