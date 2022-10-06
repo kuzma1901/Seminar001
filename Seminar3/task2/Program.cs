1794fc077211c753990cbb1ee5b5903a1ec8792f
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-06 16:26:13 +0300

    решил задачи семинара3

--- Seminar3/task2/Program.cs
+++ Seminar3/task2/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Задача 20
+// Расстояние в 2D
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int numAX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int numAY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int numBX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int numBY = Convert.ToInt32(Console.ReadLine());
+
+double LenghtLine (int ax, int ay, int bx, int by)
+{
+    if (ay == by && ax == bx) return 0;
+    int x = bx - ax;
+    int y = by - ay;
+    return Math.Sqrt (x*x + y*y);
+}
+double lenghtAB = LenghtLine(numAX, numAY, numBX, numBY);
+double lenght = Math.Round(lenghtAB, 2, MidpointRounding.ToZero);
+Console.WriteLine($"расстояние между A, B -> {lenght}");
---
+++ Seminar3/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 22. Таблица квадратов
+
+void TableSqr(int arg)
+{
+    int i = 1;
+    if (arg > 0)
+    {
+    while (i <= arg)
+    {
+        Console.WriteLine($"{i}     {Math.Pow(i, 2)}");
+        i++;
+    }    
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+    }
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+TableSqr(num);
