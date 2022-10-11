4fca913e506234a86da21d419ca3b4665218e925
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-11 14:56:38 +0300

    Решил Дз к семинару 5

---
+++ HomeWorkSeminar5/task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36:
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int SumOddElem(int[] array)
+{
+    int sumOdd = default;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 == 1) sumOdd += array[i];
+
+    return sumOdd;
+}
+int[] arr = CreateArrayRndInt(5, 1, 10);
+PrintArray(arr);
+int resultSum = SumOddElem(arr);
+Console.WriteLine($"Сумма элементов на нечетных позициях {resultSum}");
---
+++ HomeWorkSeminar5/task38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] array = new double[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.NextDouble() * (max - min) + min;
+
+    }
+
+    return array;
+
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+
+    Console.WriteLine("]");
+}
+
+
+double[] arr = CreateArrayRndInt(5, 2, 10);
+PrintArray(arr);
+
+
+double maxValue = arr.Max();
+double minValue = arr.Min();
+
+double diff = maxValue - minValue;
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива - {diff}");
