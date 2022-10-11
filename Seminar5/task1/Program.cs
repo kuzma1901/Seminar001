ccd0076015e8e4b29a08086c55fcd009b5ec6d3f
Андрей Кузнецов <kuznetsov14287917@mail.ru>
2022-10-11 12:43:03 +0300

    Решил задачи семинара

---
+++ Seminar5/task1/Program.cs
@@ -0,0 +1,110 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна -20.
+
+// Задаем массив:
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+// Ищем сумму общим методом:
+
+int[] GetSumPositiveNegativeElem(int[] array)
+{
+    int sumPositive = default;
+    int sumNegative = default;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sumNegative += array[i];
+        else sumPositive += array[i];
+    }
+    return new int[] { sumPositive, sumNegative };
+}
+
+
+// Ищем сумму двумя отдельными методами:
+
+int GetSumPositiveElem(int[] array)
+{
+    int sumPositive = default;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) sumPositive += array[i];
+    return sumPositive;
+}
+int GetSumNegativeElem(int[] array)
+{
+    int sumNegative = default;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0) sumNegative += array[i];
+    return sumNegative;
+}
+// Размер массива
+
+int[] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+
+// Вывод результат в консоль общего метода (не желательно)
+
+int[] sumPositiveNegativeElem = GetSumPositiveNegativeElem(arr);
+Console.WriteLine($"Сумма положительных элементов = {sumPositiveNegativeElem[0]}");
+Console.WriteLine($"Сумма отрицательных элементов = {sumPositiveNegativeElem[1]}");
+
+// Вызов результата в консоль по двум методам
+
+int sumPosit = GetSumPositiveElem(arr);
+int sumNegat = GetSumNegativeElem(arr);
+Console.WriteLine($"Сумма положительных элементов = {sumPosit}");
+Console.WriteLine($"Сумма отрицательных элементов = {sumNegat}");
+
+// Массив вещественных чисел
+
+// double[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     double[] array = new double[size];
+//     var rnd = new Random();
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = rnd.NextDouble() * (max - min) + min;
+
+//     }
+    
+//     return array;
+// }
+// void PrintArray(double[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+//         else Console.Write($"{array[i]}");
+//     }
+
+//     Console.WriteLine("]");
+// }
+
+// double[] arr = CreateArrayRndInt(5, 2, 10);
+// PrintArray(arr);
---
+++ Seminar5/task3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindElemArray(int[] array, int findInt)
+{
+    bool result = false;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == findInt)
+        {
+            result = true;
+            break;
+        }
+        return result;
+}
+
+int[] arr = CreateArrayRndInt(10, -5, 5);
+
+PrintArray(arr);
+Console.Write("Введите искомое число в массиве: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(FindElemArray(arr, num)) Console.WriteLine("число есть в массиве");
+else Console.WriteLine("числа нет в массиве");
